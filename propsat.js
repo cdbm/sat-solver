b399526ee2f6c3a0613bc0282d1fbb75f2c1dbbd
cdbm <cdbm@cin.ufpe.br>
2019-09-01 17:53:29 -0300

    sat encapsulado em um servidor express

--- propsat.js
+++ propsat.js
@@ -1,3 +1,13 @@
 // roda o satsolver
 const sat = require ('./sat.js')
-console.log(sat.solve('./testcases/simple0.cnf'))
+
+const express = require('express')
+const app = express()
+ 
+console.log("app listenning on port 5000")
+app.get('/', function (req, res) {
+  res.send(sat.solve('./testcases/simple2.cnf'))
+
+})
+ 
+app.listen(5000)
